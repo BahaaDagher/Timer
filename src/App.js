3ddc1dbbd139ae9ef0fc1948b8d3b1b8a92b9bdd
BahaaDagher <bahaa.dagher99@gmail.com>
2023-04-19 21:31:16 +0200

    create react app

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css' ; 
+import 'bootstrap/dist/js/bootstrap.min.js' ; 
+import Timer from './components/Timer.jsx';
+function App() {
+  return (
+    <Timer></Timer>
+  );
+}
+
+export default App;
